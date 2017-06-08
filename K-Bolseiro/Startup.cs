ebe0479c3928ad2f0c55862aa1028fd07e85f7c0
nlsantos <nelson.santos@students.plymouth.ac.uk>
2017-06-08 11:14:07 +0100

    Add project files.

---
+++ K-Bolseiro/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(K_Bolseiro.Startup))]
+namespace K_Bolseiro
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
