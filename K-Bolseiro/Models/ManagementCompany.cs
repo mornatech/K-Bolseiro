367a6042805b2678fe2a2093d14e2b58bd4c69f3
nlsantos <nelson.santos@students.plymouth.ac.uk>
2017-06-09 18:16:51 +0100

    added management company and staff and abstract person class

---
+++ K-Bolseiro/Models/ManagementCompany.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace K_Bolseiro.Models
+{
+    public class ManagementCompany
+    {
+        public int ID { get; set; }
+        public string name { get; set; }
+        public string address1 { get; set; }
+        public string address2 { get; set; }
+        public string city { get; set; }
+        public string postCode { get; set; }
+        public string country { get; set; }
+        public string telephone { get; set; }
+        public virtual ICollection<ManagingCompanyStaff> ManagingStaff { get; set; }
+        
+    }
+}
---
+++ K-Bolseiro/Models/ManagingCompanyStaff.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace K_Bolseiro.Models
+{
+    public class ManagingCompanyStaff : Person
+    {
+        
+        public string email { get; set; }
+        public string department { get; set; }
+        public bool admin { get; set; }
+
+        public virtual ManagementCompany ManagementCompany { get; set; }
+         
+    }
+}
